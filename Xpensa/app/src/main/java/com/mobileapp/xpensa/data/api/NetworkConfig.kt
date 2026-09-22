2cea2a0165f14e1368491209b5a87fc27c6285d9
Gianluca Sanfilippo <pagiansal68@gmail.com>
2026-09-22 16:12:52 +0200

    update ip address for cloud endpoint

--- Xpensa/app/src/main/java/com/mobileapp/xpensa/data/api/NetworkConfig.kt
+++ Xpensa/app/src/main/java/com/mobileapp/xpensa/data/api/NetworkConfig.kt
@@ -2,5 +2,8 @@ package com.mobileapp.xpensa.data.api
 
 object NetworkConfig {
     // Sostituisci qui l'IP quando cambi rete/macchina
-    const val BACKEND_BASE_URL = "http://192.168.1.16:8000/"
+    //const val BACKEND_BASE_URL = "http://192.168.1.16:8000/"
+    //const val BACKEND_BASE_URL = "http://192.168.17.113:8000/"
+    const val BACKEND_BASE_URL = "http://ec2-35-159-239-25.eu-central-1.compute.amazonaws.com"
+
 }
